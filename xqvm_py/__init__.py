917cc8712f11d7d76f970c25001f741291dad3da
Manish Meganathan <meganathan.manish@gmail.com>
2026-09-25 22:29:41 +0530

    chore: reopen main at 0.4.2-dev
    Signed-off-by: Manish Meganathan <meganathan.manish@gmail.com>

--- xqvm_py/__init__.py
+++ xqvm_py/__init__.py
@@ -28,7 +28,7 @@ runtime); see the package README for the full status note.
 
 from __future__ import annotations
 
-__version__ = "0.4.1"
+__version__ = "0.4.2.dev0"
 
 from .errors import (
     ArithmeticOverflow,
